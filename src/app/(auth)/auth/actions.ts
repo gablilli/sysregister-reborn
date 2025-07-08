89455952f2bf66dd8601f191623e39ca3bd99f5d
gablilli <167802854+gablilli@users.noreply.github.com>
2025-07-08 15:55:29 +0200

    fix

--- src/app/(auth)/auth/actions.ts
+++ src/app/(auth)/auth/actions.ts
@@ -1,10 +1,10 @@
-"use server";
-import { handleAuthError } from "@/lib/api";
-import { db } from "@/lib/db";
-import { JSDOM } from "jsdom";
-import { cookies } from "next/headers";
-import { SignJWT } from "jose";
-import { getUserDetailsFromToken } from "@/lib/utils";
+'use server';
+
+import { db } from '@/lib/db';
+import { cookies } from 'next/headers';
+import { SignJWT } from 'jose';
+import { getUserDetailsFromToken } from '@/lib/utils';
+import { JSDOM } from 'jsdom';
 
 export async function getUserSession({ uid, pass }: { uid: string, pass: string }) {
     if (!uid || !pass) {
@@ -13,23 +13,18 @@ export async function getUserSession({ uid, pass }: { uid: string, pass: string 
 
     const formData = new FormData();
     formData.append("uid", uid);
-    formData.append("pass", pass); // Fixato da pwd a pass
+    formData.append("pwd", pass);
 
-    let req;
-    try {
-        req = await fetch("https://web.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd", {
-            method: "POST",
-            body: formData
-        });
-    } catch {
-        return "Errore di connessione a Spaggiari.";
-    }
+    const req = await fetch("https://web.spaggiari.eu/auth-p7/app/default/AuthApi4.php?a=aLoginPwd", {
+        method: "POST",
+        body: formData,
+    });
 
-    const cookieHeader = req.headers.get("set-cookie") || "";
-    const match = cookieHeader.match(/PHPSESSID=([a-zA-Z0-9]+)/);
-    const token = match?.[1];
+    const setCookies = req.headers.get("set-cookie")?.split("; ");
+    const expiry = req.headers.get("expires");
+    const token = setCookies?.find(cookie => cookie.startsWith("HttpOnly, PHPSESSID="))?.split("=")[1];
 
-    if (!token) {
+    if (!token || !expiry) {
         return "Credenziali non valide.";
     }
 
@@ -48,6 +43,53 @@ export async function getUserSession({ uid, pass }: { uid: string, pass: string 
         .sign(secret);
 
     cookies().set("internal_token", tokenJwt);
+    cookies().set("tokenExpiry", new Date(expiry).toISOString());
     cookies().set("token", token);
-    cookies().set("tokenExpiry", new Date(Date.now() + 2 * 60 * 60 * 1000).toISOString()); // fallback
+}
+
+export async function getUserDetails() {
+    const userData = await getUserDetailsFromToken(cookies().get("internal_token")?.value || "");
+    if (!userData) return null;
+
+    const page = await (await fetch("https://web.spaggiari.eu/home/app/default/menu_webinfoschool_genitori.php", {
+        headers: {
+            Cookie: `PHPSESSID=${cookies().get("token")?.value}; webidentity=${userData.uid};`,
+        },
+    })).text();
+
+    const dom = new JSDOM(page);
+
+    try {
+        const schoolName = dom.window.document.querySelector("span.scuola")?.textContent;
+        return {
+            schoolName
+        };
+    } catch {
+        return null;
+    }
+}
+
+export async function verifySession() {
+    const userData = await getUserDetailsFromToken(cookies().get("internal_token")?.value || "");
+    if (!userData) return false;
+
+    const page = await (await fetch("https://web.spaggiari.eu/home/app/default/menu_webinfoschool_genitori.php", {
+        headers: {
+            Cookie: `PHPSESSID=${cookies().get("token")?.value}; webidentity=${userData.uid};`,
+        },
+    })).text();
+
+    const dom = new JSDOM(page);
+
+    try {
+        if (dom.window.document.querySelector("span.scuola")) {
+            const internalUser = await db.user.findUnique({
+                where: { id: userData.uid }
+            });
+            return internalUser?.internalId === userData.internalId;
+        }
+        return false;
+    } catch {
+        return false;
+    }
 }
